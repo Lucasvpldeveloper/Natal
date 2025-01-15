8544770fb89fb5090a1e10db9c45b503f8fa12bc
Lucasvpl <lucasvpldeveloper@gmail.com>
2025-01-15 01:44:54 -0300

    projeto finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,26 @@
-
-
+import './style.module.scss'
+import linkedin from '../assets/linkedin.png'
+import instagram from '../assets/instagram.png'
+import github from '../assets/github.png'
+import presente from '../assets/presente.png'
 
 export default function Footer() {
     return(
         <footer>
-            footer
+            <div>
+                <img src={linkedin} alt="Logo linkedin" />
+                <img src={instagram} alt="Logo instagram" />
+                <img src={github} alt="Logo GitHub" />
+            </div>
+            <nav>
+                <p>Lucas Vinicius</p>
+                <p>Instrutor: Vinicius</p>
+                <p>Facilitadora: Marina</p>
+                <p>Layout criado para fins natalistícos na escola vai na web</p>
+            </nav>
+            <section>
+                <img src={presente} alt="Presentes" />
+            </section>
         </footer>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,25 @@
 import principalMain from '../assets/principalMain.png'
+import arvoreGlobo from '../assets/arvoreGlobo.png'
+import cidade1 from '../assets/cidade1.png'
+import cidade2 from '../assets/cidade2.png'
+import cidade3 from '../assets/cidade3.png'
+import S from './style.module.scss'
 
 
 export default function Main() {
     return(
         <main>
-            <section className='teste'>
+            <section className={S.imgPrincipal}>
                 <img src={principalMain} alt="Papai noel fazendo presentes" />
+            </section>
+            <section className={S.origemBox}>
+                <img src={arvoreGlobo} alt="Globo de natal com arvore dentro" />
+                <p>A origem do globo de neve é incerta, mas acredita-se que tenha surgido na Europa no século XIX, tornando-se um símbolo de magia, afeto e encantamento, especialmente no Natal, trazendo um toque de nostalgia e maravilha para as celebrações e encantando gerações ao redor do mundo.</p>
+            </section>
+            <section className={S.imagensBox}>
+                <img src={cidade1} alt="Cidades para viajar no natal" />
+                <img src={cidade2} alt="Cidade para viajar no natal" />
+                <img src={cidade3} alt="Cidade para viajar no natal" />
             </section>
         </main>
     )
